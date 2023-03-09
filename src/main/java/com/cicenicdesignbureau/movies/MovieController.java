652b84a7d5bad0aa51a5b2d384523e191b862263
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-03-09 20:38:48 +0300

    updated the cors link

--- src/main/java/com/cicenicdesignbureau/movies/MovieController.java
+++ src/main/java/com/cicenicdesignbureau/movies/MovieController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
-@CrossOrigin("localhost:3000/")
+@CrossOrigin("http://localhost:3000/")
 public class MovieController {
 
     @Autowired
